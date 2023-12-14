570709cbd92e8d3219c593de832e764cc5a47d7b
Clayton Lott <claylottjr@gmail.com>
2023-12-13 19:40:09 -0600

    origin

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { puppyList } from './data.js'
+import { useState } from 'react'
+///console.log(puppyList);
+
+
 
 
 function App() {
-  return <div></div>
-
+  
+  const [puppies, setPuppies] = useState(puppyList);
+  return <div>{puppies.map((puppy) => {
+    return <p key={puppy.id}>{puppy.name}</p>
+  })}</div>
 }
 
 export default App
